07b9472bf182b98484129cb461b6daba8605dfcf
Ferran <ferri.fc@gmail.com>
2021-02-23 16:23:13 +0100

    senseair update

--- SenseAir_S8.h
+++ SenseAir_S8.h
@@ -1,23 +1,39 @@
+// AirGradient Arduino Library for ESP8266
+// =======================================
+// This library makes it easy to read the sensor data from the Plantower PMS5003 PM2.5 sensor,
+// the Senseair S8 and the SHT30/31 Temperature and Humidity sensor.
+// Visit our blog for detailed build instructions and PCB layout.
+// https://www.airgradient.com/blog/
+// https://www.arduino.cc/reference/en/libraries/airgradient-air-quality-sensor/
+
 #include <SoftwareSerial.h>
 
 void CO2_Init(int rx_pin, int tx_pin, int baudRate);
-int getCO2_Raw();
+int getCO2();
 
-SoftwareSerial *_SoftSerial_CO2;
-char Char_CO2[10];
+SoftwareSerial *SoftSerial_CO2;
 
-struct CO2_READ_RESULT {
-    int co2 = -1;
-    bool success = false;
-};
+/*
+void setup() {
+   Serial.begin(9600);
+   CO2_Init(D6, D7, 9600);
+}
+
+void loop() {
+   int CO2 = getCO2();
+   Serial.print("C02: ");
+   Serial.println(CO2);
+   delay(5000);
+}
+*/
 
 //START CO2 FUNCTIONS //
 void CO2_Init(int rx_pin, int tx_pin, int baudRate) {
    Serial.println("Initializing CO2 ...");
-   _SoftSerial_CO2 = new SoftwareSerial(rx_pin,tx_pin);
-   _SoftSerial_CO2->begin(baudRate);
+   SoftSerial_CO2 = new SoftwareSerial(rx_pin, tx_pin);
+   SoftSerial_CO2->begin(baudRate);
 
-   if (getCO2_Raw() == -1) {
+   if (getCO2() == -1) {
       Serial.println("CO2 Sensor Failed to Initialize ");
    }
    else {
@@ -26,38 +42,36 @@ void CO2_Init(int rx_pin, int tx_pin, int baudRate) {
    }
 }
 
-int getCO2_Raw() {
-   int retry = 0;
-   CO2_READ_RESULT result;
+int getCO2() {
    const byte CO2Command[] = {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25};
    byte CO2Response[] = {0,0,0,0,0,0,0};
 
-   while (!(_SoftSerial_CO2->available())) {
+   int retry = 0;
+   while (!(SoftSerial_CO2->available())) {
       retry++;
       // keep sending request until we start to get a response
-      _SoftSerial_CO2->write(CO2Command, 7);
+      SoftSerial_CO2->write(CO2Command, 7);
       delay(50);
       if (retry > 10) {
          return -1;
       }
    }
 
-   int timeout = 0; 
-    
-   while (_SoftSerial_CO2->available() < 7) {
+   int timeout = 0;
+   while (SoftSerial_CO2->available() < 7) {
       timeout++; 
       if (timeout > 10) {
-         while(_SoftSerial_CO2->available())  
-            _SoftSerial_CO2->read();
-            break;                    
+         while(SoftSerial_CO2->available()) {
+            SoftSerial_CO2->read();
+         }
+         break;                    
       }
       delay(50);
    }
 
    for (int i=0; i < 7; i++) {
-      int byte = _SoftSerial_CO2->read();
+      int byte = SoftSerial_CO2->read();
       if (byte == -1) {
-         result.success = false;
          return -1;
       }
       CO2Response[i] = byte;
@@ -65,5 +79,4 @@ int getCO2_Raw() {
 
    return CO2Response[3]*256 + CO2Response[4];
 }
-
 //END CO2 FUNCTIONS //
